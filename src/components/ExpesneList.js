5882564453d8c824d6b22eb2ce4b5c37d1a3fe9d
soyello <wodus9193@naver.com>
2024-07-29 00:02:52 +0900

    Create project

---
+++ src/components/ExpesneList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./ExpenseList.css";
+import ExpenseItem from "./ExpenseItem";
+import { MdDelete } from "react-icons/md";
+
+const ExpenseList = ({expenses,handleDelete,handleEdit,clearItems}) => {
+
+  return (
+    <>
+      <ul className="list">
+        {/* Expense Item */}
+        {expenses.map((expense) => {
+          return (
+            <ExpenseItem
+              expense={expense}
+              key={expense.id}
+              handleDelete={handleDelete}
+              handleEdit={handleEdit}
+            />
+          );
+        })}
+        {expenses.length > 0 && (
+          <button className="btn" onClick={clearItems}>
+            목록 지우기
+          <MdDelete className="btn-icon" />
+        </button>
+        )}
+      </ul>
+      
+    </>
+  );
+};
+
+export default ExpenseList;
